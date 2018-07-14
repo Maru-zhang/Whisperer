7f8e75000fb2aef8650d898e8b466bcafcf86ffb
Maru <zhangbinhui@souche.com>
2018-07-15 03:08:26 +0800

    feat: 新增debug模式

--- whisperer.py
+++ whisperer.py
@@ -14,7 +14,8 @@ from markdown2 import Markdown
 
 @click.command()
 @click.argument('target')
-def cli(target):
+@click.option('--debug', type=click.BOOL, default=False, help='是否为DEBUG模式.')
+def cli(target, debug):
     now = datetime.now()
     year = now.strftime('%Y')
     week = now.strftime('%U')
@@ -31,7 +32,7 @@ def cli(target):
     while flag != 'yes':
         flag = input(r'请问你编辑完成了么(yes/no)?\n')
     whisperer = Whisperer()
-    whisperer.run(target, file_path)
+    whisperer.run(target, file_path, debug)
     click.echo('发送成功!')
 
 class Whisperer():
@@ -47,17 +48,23 @@ class Whisperer():
     # 配置文件名
     config_name = '.whisperer.ini'
     # HOME目录
-    workspace = str(Path.home())
+    workspace = str(Path.home()) + '/.whisperer'
     # 配置解析
     cfg = ConfigParser()
+    # 是否处于DEBUG
+    debug = False
 
     def prepare_setup(self):
-        logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.WARNING)
+        if self.debug is True:
+            logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+        else:
+            logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.WARNING)
         name = self.config_name
         path = self.workspace + f'/{name}'
         if os.path.exists(path) is False:
+            os.mkdir(self.workspace)
             self.my_email_address = input('请输入您的邮箱地址: ')
-            self.my_email_password = input('请输入您的邮箱: ')
+            self.my_email_password = input('请输入您的邮箱密码: ')
             self.cfg.add_section('base')
             self.cfg.set('base', 'my_email_address', self.my_email_address)
             self.cfg.set('base', 'my_email_password', self.my_email_password)
@@ -73,7 +80,6 @@ class Whisperer():
     def build_email(self, html):
         message = MIMEText(html, 'html', 'utf-8')
         message['From'] = formataddr(['张斌辉', self.my_email_address])
-        message['To'] =  Header("TL", 'utf-8')
         subject = '张斌辉 周报'
         message['Subject'] = Header(subject, 'utf-8')
         return message
@@ -88,11 +94,13 @@ class Whisperer():
 
     def send_email(self, to_address, message):
         server = smtplib.SMTP(self.smtp_server, timeout=10)
+        server.set_debuglevel(self.debug)
         server.login(self.my_email_address, self.my_email_password)
         server.sendmail(self.my_email_address, [to_address], message.as_string())
         server.quit()
 
-    def run(self, target, source):
+    def run(self, target, source, debug):
+        self.debug = debug
         self.prepare_setup()
         email = self.build_email_from_markdown(source)
         self.send_email(target, email)
