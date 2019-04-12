341424c02c0a6b4e6c880c70512741b4839b88a8
Maru <zhangbinhui@souche.com>
2019-04-12 17:16:38 +0800

    refactor: 添加发布脚本工具

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Whisperer',
-    version='0.1.0',
+    version='0.2.0',
     author='zhangbinhui',
     py_modules=['whisperer'], 
     install_requires=['Click', 'markdown2', 'python-gitlab'],
